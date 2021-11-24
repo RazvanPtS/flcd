111f2ce363f751991930f9304bd9f04be90d4c28
Razvan <tirdeaandrei@gmail.com>
2021-11-24 10:47:12 +0200

    Lab5-not completed

--- lab5/src/Main.java
+++ lab5/src/Main.java
@@ -1,18 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         Grammar grammar = readGrammar();
         Scanner input = new Scanner(System.in);
-        System.out.println("1-Non-terminals\n2-Terminals\n3-productions\n4-initial states\n5-Productions for non-terminal\n6-CFG check\n0-exit");
+        System.out.println("1-Non-terminals\n2-Terminals\n3-productions\n4-initial states\n5-Productions for non-terminal\n6-CFG check\n7-FIRST\n8-FOLLOW\n0-exit");
         String in = input.nextLine();
         while(!in.equals("0")){
             switch (in){
@@ -34,12 +32,45 @@ public class Main {
                 case "6":
                     System.out.println(checkCFG(grammar) ? "CFG" : "Non-cfg");
                     break;
+                case "7":
+                    getFirst(grammar);
+                    break;
+                case "8":
+                    //getFollow(grammar);
+                    break;
                 case "0":
                     return;
             }
-            System.out.println("1-Non-terminals\n2-Terminals\n3-productions\n4-initial states\n5-Productions for non-terminal\n6-CFG check\n0-exit");
+            System.out.println("1-Non-terminals\n2-Terminals\n3-productions\n4-initial states\n5-Productions for non-terminal\n6-CFG check\n7-FIRST\n8-FOLLOW\n0-exit");
             in = input.nextLine();
         }
+    }
+
+    private static void getFirst(Grammar grammar) {
+        Map<String, List<String>> first = new HashMap<>();
+        List<String> terminals = grammar.getTerminals();
+
+        //init
+
+        terminals.forEach(terminal->
+            first.put(terminal, List.of(terminal)));
+
+        grammar.getNonTerminals().forEach(nonT->{
+            List<Production> productions = grammar.getProductions().stream().filter(pr->pr.getInitState().equals(nonT)).collect(Collectors.toList());
+            for(Production p : productions) {
+                String finalP = p.getFinalState();
+                for (int i = 0; i < finalP.length(); i++) {
+                    if (terminals.contains(String.valueOf(finalP.charAt(i)))) {
+                        first.put(nonT, List.of(String.valueOf(finalP.charAt(i))));
+                        break;
+                    }
+                }
+                if (!first.containsKey(nonT)) {
+                    first.put(nonT, List.of());
+                }
+            }
+        });
+        first.forEach((k,v)->System.out.println(k+"->"+v));
     }
 
     private static boolean checkCFG(Grammar grammar) {
